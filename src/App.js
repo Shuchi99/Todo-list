8b49a77c89c1b1355d52f5ccf9f9baae7105113c
shuchi mandal <shuchi1999@gmail.com>
2025-01-29 22:07:54 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Todo App</h1>
+        {/* <TodoForm></TodoForm> */}
+        <TodoList></TodoList>
+      </header>
+    </div>
+  );
+}
+
+export default App;
